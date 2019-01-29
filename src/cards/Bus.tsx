4349fabff1c8a2150cfb55f8c2fc742ebb04b3b3
LaySent <laysent@gmail.com>
2019-01-30 00:11:13 +0800

    add: start/pause button for constantly fetching bus status

--- src/cards/Bus.tsx
+++ src/cards/Bus.tsx
@@ -2,11 +2,27 @@ import React, { PureComponent } from 'react';
 import Card, { icons } from './Card';
 import getBusDetails, { busType } from '../utils/bus';
 import * as helper from '../utils/helper';
+import loading from '../img/loading.svg';
+import play from '../img/play.svg';
+import pause from '../img/pause.svg';
+
+const interval = 30 * 1000;
 
 class Bus extends PureComponent {
-  state = {
-    buses: [],
-    loading: true,
+  state: {
+    buses: Array<number>;
+    fetching: boolean;
+    paused: boolean;
+  }
+  constructor(props: { }) {
+    super(props);
+    const time = helper.getTime();
+    const catchingBus = time === helper.time.morning || time === helper.time.evening;
+    this.state = {
+      buses: [],
+      fetching: false,
+      paused: !catchingBus,
+    };
   }
   stop: (() => void) | null = null;
   getBusDetails = async () => {
@@ -14,19 +30,38 @@ class Bus extends PureComponent {
     if (time === helper.time.night) {
       this.setState({
         buses: [],
-        loading: false,
+        fetching: false,
       });
       return;
     }
     const type = time === helper.time.morning ? busType.toCompany : busType.fromCompany;
+    this.setState({
+      fetching: true,
+    });
     const buses = await getBusDetails(type);
     this.setState({
       buses: buses.map(seconds => Math.round(seconds / 60)),
-      loading: false,
+      fetching: false,
+    });
+  }
+  onStatusChange = () => {
+    const paused = !this.state.paused;
+    if (paused) {
+      if (this.stop !== null) this.stop();
+      this.stop = null;
+    } else {
+      this.stop = helper.setIntervalWhenActive(this.getBusDetails, interval);
+    }
+    this.setState({
+      paused,
     });
   }
   componentDidMount() {
-    this.stop = helper.setIntervalWhenActive(this.getBusDetails, 30 * 1000);
+    if (this.state.paused) {
+      this.getBusDetails();
+    } else {
+      this.stop = helper.setIntervalWhenActive(this.getBusDetails, interval);
+    }
   }
   componentWillUnmount() {
     if (this.stop !== null) this.stop();
@@ -35,7 +70,7 @@ class Bus extends PureComponent {
     const buses = this.state.buses;
     let title = 'Go Texi';
     if (buses.length > 0) title = `${buses[0]} min`;
-    else if (this.state.loading) title = '-';
+    else if (this.state.fetching) title = '-';
     return (
       <Card
         color="#f9d171"
@@ -43,7 +78,19 @@ class Bus extends PureComponent {
         subtitle={buses.length > 1 ? `${buses.slice(1).map(bus => `${bus} min`).join('/')}` : ''}
         icon={icons.bus}
         loading={false}
-      />
+      >
+        {this.state.fetching ? (
+          <img src={loading} alt="fetching" className="small-icon right-top spinning" />
+        ) : (
+          <button className="icon-button right-top" onClick={this.onStatusChange}>
+            <img
+              src={this.state.paused ? play : pause}
+              alt={this.state.paused ? 'play' : 'pause'}
+              className="small-icon"
+            />
+          </button>
+        )}
+      </Card>
     );
   }
 }
